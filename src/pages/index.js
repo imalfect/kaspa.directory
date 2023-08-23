54e50d2005291f9ca3ec0f57d8bf39fd3dc35d84
iMalFect <me@imal.dev>
2023-08-23 22:58:06 +0200

    Change title and description in meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`kaspa.directory`}
+      description="kaspa.directory is a website, which allows you to check out all the projects within the kaspa ecosystem, as well as funding pools and the community members!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
